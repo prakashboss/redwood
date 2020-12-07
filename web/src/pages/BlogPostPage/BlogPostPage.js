b42a7245228468ab7a99b55b1c167a76ffb6b2e1
Prakash Baskaran <b.prakashboss@gmail.com>
2020-12-08 00:03:02 +1300

    https://redwoodjs.com/tutorial/routing-params#using-the-param

---
+++ web/src/pages/BlogPostPage/BlogPostPage.js
@@ -0,0 +1,14 @@
+import { Link, routes } from '@redwoodjs/router'
+
+const BlogPostPage = () => {
+  return (
+    <>
+      <h1>BlogPostPage</h1>
+      <p>
+        Find me in <code>./web/src/pages/BlogPostPage/BlogPostPage.js</code>
+      </p>
+    </>
+  )
+}
+
+export default BlogPostPage
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,9 +1,11 @@
 import BlogLayout from 'src/layouts/BlogLayout'
+import BlogPostsCell from 'src/components/BlogPostsCell'
 
 const HomePage = () => {
   return (
     <>
       <BlogLayout>
+        <BlogPostsCell />
         <p>This is the Home page</p>
       </BlogLayout>
     </>
