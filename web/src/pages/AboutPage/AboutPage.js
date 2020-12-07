102776c64213371b93f3c778c4d3cf75be915fbb
Prakash Baskaran <b.prakashboss@gmail.com>
2020-12-07 18:39:42 +1300

    done the first video till layouts

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,14 @@
+import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+
+const AboutPage = () => {
+  return (
+    <>
+      <BlogLayout>
+        <p>This is AboutPage</p>
+      </BlogLayout>
+    </>
+  )
+}
+
+export default AboutPage
